b51beefb822fcc8d2ab21e89a8f0180c3869565f
mayconbalves <mayconmustaine@hotmail.com>
2019-02-05 21:36:20 -0200

    feat(*): create index of app

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+mongoose.connect('mongodb://gooweek:admin123@ds221435.mlab.com:21435/goweek-maycon-backend', {
+  useNewUrlParser: true
+})
+
+app.get('/', (req, res) => {
+  return res.send('hello world')
+})
+
+app.listen(3000,  () => {
+  console.log(`server run on port 3000`)
+})
