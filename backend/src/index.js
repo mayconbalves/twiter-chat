5f158375b37051ba006bbda84d5acb1ba3fd124f
mayconbalves <mayconmustaine@hotmail.com>
2019-02-05 22:42:18 -0200

    feat(index): add io and ser consts

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
 mongoose.connect('mongodb://gooweek:admin123@ds221435.mlab.com:21435/goweek-maycon-backend', {
   useNewUrlParser: true
 })
 
+app.use((req, res, next) => {
+  req.io = io
+  return next()
+})
+
+app.use(cors())
 app.use(express.json())
 app.use(require('./routes'))
 
-app.listen(3000,  () => {
+server.listen(3000,  () => {
   console.log(`server run on port 3000`)
 })
