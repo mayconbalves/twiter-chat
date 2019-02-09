5dbf93a716190783e64f638cc75d7f3f4363495f
mayconbalves <mayconmustaine@hotmail.com>
2019-02-09 10:39:46 -0200

    feat(tweet): create function for likes

--- oministeck/src/components/Tweet.js
+++ oministeck/src/components/Tweet.js
@@ -6,9 +6,17 @@ import {
   TouchableOpacity
 } from 'react-native'
 
+import api  from '../services/api'
+
 import Icon from 'react-native-vector-icons/Ionicons'
 
 class Tweet extends Component {
+
+  handleLike = () => {
+    const { _id } = this.props.tweet
+
+    api.post(`likes/${_id}`)
+  }
   render() {
     const { tweet } = this.props
     return (
@@ -16,7 +24,7 @@ class Tweet extends Component {
         <Text style={styles.author}>{tweet.author}</Text>
         <Text style={styles.content}>{tweet.content}</Text>
 
-        <TouchableOpacity onPress={() => {}} style={styles.likeButton}>
+        <TouchableOpacity onPress={this.handleLike} style={styles.likeButton}>
           <Icon name='ios-heart-empty' size={20} color='#999' />
           <Text style={styles.likeText}>{tweet.likes}</Text>
         </TouchableOpacity>
