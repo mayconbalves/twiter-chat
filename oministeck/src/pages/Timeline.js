289cac258a0d385a136ffa6e5632bdeae9fab272
mayconbalves <mayconmustaine@hotmail.com>
2019-02-09 10:41:24 -0200

    feat(timeline): add functions navigate for new tweet

--- oministeck/src/pages/Timeline.js
+++ oministeck/src/pages/Timeline.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  FlatList
+} from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import api  from '../services/api'
+import Tweet from '../components/Tweet'
 class Timeline extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Inicio',
+    headerRight: (
+      <TouchableOpacity onPress={() => navigation.navigate('New')}>
+        <Icon
+          name='add-circle-outline'
+          size={24}
+          color='#4BB0EE'
+          style={{ marginRight: 20 }}
+        />
+      </TouchableOpacity>
+    )
+  })
+
+  state = { tweets: [] }
+
+  async componentDidMount () {
+    const response = await api.get('tweets')
+
+    this.setState({ tweets: response.data })
+  }
   render() {
     return (
-      <View>
-        <Text>Timeline</Text>
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.tweets}
+          keyExtractor={tweet => tweet._id}
+          renderItem={({ item }) => <Tweet  tweet={item} />}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  }
+});
+
 
   export default Timeline
