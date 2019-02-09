bf2cf5c155e43a2d7fea2a834838d35aa97a7144
mayconbalves <mayconmustaine@hotmail.com>
2019-02-08 23:21:07 -0200

    feat(pages): create timeline

---
+++ oministeck/src/pages/Timeline.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+class Timeline extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Timeline</Text>
+      </View>
+    )
+  }
+}
+
+  export default Timeline
