a7bc3ef26534ae6fa57d36f6bf266f3a4d3bba67
mayconbalves <mayconmustaine@hotmail.com>
2019-02-10 10:56:56 -0200

    chore(index): remove warning alert

--- oministeck/src/index.js
+++ oministeck/src/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import Routes from './routes'
+import { YellowBox } from 'react-native'
+
+YellowBox.ignoreWarnings(['Unrecognized WebSocket'])
 
 const App = () => <Routes />
 
